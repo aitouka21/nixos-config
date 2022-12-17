f4339bd67bc8dff30d1149264c68b6be9cc4c324
aitouka <wscheungae@gmail.com>
2022-12-17 17:38:53 +0800

    feat: alacritty color scheme

--- home/alacritty.nix
+++ home/alacritty.nix
@@ -11,46 +11,35 @@
 
       colors = {
         primary = {
-          background = "0x241e3d";
-          foreground = "0xc0bbcb";
+          background = "#241e3d";
+          foreground = "#c0bbcb";
         };
 
         cursor = {
-          text = "0x241e3d";
-          cursor = "0xc0bbcb";
+          text = "#241e3d";
+          cursor = "#c0bbcb";
         };
 
         normal = {
-          black = "0x241e3d";
-          red = "0xd54e53";
-          green = "0xb9ca4a";
-          yellow = "0xe6c547";
-          blue = "0x5b81f3";
-          magenta = "0xc397d8";
-          cyan = "0x70c0ba";
-          white = "0xffffff";
+          black = "#444a73";
+          red = "#fe719e";
+          green = "#63ee92";
+          yellow = "#fad13d";
+          blue = "#8087ee";
+          magenta = "#fc7b7b";
+          cyan = "#86e1fc";
+          white = "#d0d0d0";
         };
 
         bright = {
-          black = "0x241e3d";
-          red = "0xff3334";
-          green = "0x9ec400";
-          yellow = "0xe7c547";
-          blue = "0x7aa6da";
-          magenta = "0xb77ee0";
-          cyan = "0x54ced6";
-          white = "0xffffff";
-        };
-
-        dim = {
-          black = "0x333333";
-          red = "0xf2777a";
-          green = "0x99cc99";
-          yellow = "0xffcc66";
-          blue = "0x6699cc";
-          magenta = "0xcc99cc";
-          cyan = "0x66cccc";
-          white = "0xdddddd";
+          black = "#828bb8";
+          red = "#ff98a4";
+          green = "#c3e88d";
+          yellow = "#d1b959";
+          blue = "#75bcff";
+          magenta = "#ff966c";
+          cyan = "#b4f9f8";
+          white = "#b6b2c0";
         };
       };
     };
