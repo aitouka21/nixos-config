db0f886736f2bfc0fc5791169d2297e4b12c2e0f
aitouka <wscheungae@gmail.com>
2023-04-22 02:52:37 +0800

    add support ntfs

--- config/home-pc/default.nix
+++ config/home-pc/default.nix
@@ -15,6 +15,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.supportedFilesystems = [ "ntfs" ];
 
   fileSystems."/" =
     {
